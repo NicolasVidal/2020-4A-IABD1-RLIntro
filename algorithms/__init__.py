6dbf768302a996b5b5d8b576ee1cd8091355fbf5
Nicolas Vidal <pro@nicolasvidal.fr>
2020-03-03 17:15:58 +0100

    grid_world, policy iteration and value iteration

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from policies import tabular_uniform_random_policy
+
 
 def iterative_policy_evaluation(
         S: np.ndarray,
@@ -8,13 +10,15 @@ def iterative_policy_evaluation(
         T: np.ndarray,
         Pi: np.ndarray,
         gamma: float = 0.99,
-        theta: float = 0.000001
+        theta: float = 0.000001,
+        V: np.ndarray = None
 ) -> np.ndarray:
     assert 0 <= gamma <= 1
     assert theta > 0
 
-    V = np.random.random((S.shape[0],))
-    V[T] = 0.0
+    if V is None:
+        V = np.random.random((S.shape[0],))
+        V[T] = 0.0
     while True:
         delta = 0
         for s in S:
@@ -30,3 +34,88 @@ def iterative_policy_evaluation(
         if delta < theta:
             break
     return V
+
+
+def policy_iteration(
+        S: np.ndarray,
+        A: np.ndarray,
+        P: np.ndarray,
+        T: np.ndarray,
+        gamma: float = 0.99,
+        theta: float = 0.000001
+) -> (np.ndarray, np.ndarray):
+    Pi = tabular_uniform_random_policy(S.shape[0], A.shape[0])
+    V = np.random.random((S.shape[0],))
+    V[T] = 0.0
+    while True:
+        V = iterative_policy_evaluation(S, A, P, T, Pi, gamma, theta, V)
+        policy_stable = True
+        for s in S:
+            old_action = np.argmax(Pi[s])
+            best_action = 0
+            best_action_score = -9999999999999
+            for a in A:
+                tmp_sum = 0
+                for s_p in S:
+                    tmp_sum += P[s, a, s_p, 0] * (
+                            P[s, a, s_p, 1] + gamma * V[s_p]
+                    )
+                if tmp_sum > best_action_score:
+                    best_action = a
+                    best_action_score = tmp_sum
+            Pi[s] = 0.0
+            Pi[s, best_action] = 1.0
+            if best_action != old_action:
+                policy_stable = False
+        if policy_stable:
+            break
+    return V, Pi
+
+
+def value_iteration(
+        S: np.ndarray,
+        A: np.ndarray,
+        P: np.ndarray,
+        T: np.ndarray,
+        gamma: float = 0.99,
+        theta: float = 0.000001
+) -> (np.ndarray, np.ndarray):
+    assert 0 <= gamma <= 1
+    assert theta > 0
+
+    V = np.random.random((S.shape[0],))
+    V[T] = 0.0
+    while True:
+        delta = 0
+        for s in S:
+            v_temp = V[s]
+            best_score = -9999999999
+            for a in A:
+                tmp_sum = 0
+                for s_p in S:
+                    tmp_sum += P[s, a, s_p, 0] * (
+                            P[s, a, s_p, 1] + gamma * V[s_p]
+                    )
+                if best_score < tmp_sum:
+                    best_score = tmp_sum
+            V[s] = best_score
+            delta = np.maximum(delta, np.abs(V[s] - v_temp))
+        if delta < theta:
+            break
+
+    Pi = np.zeros((S.shape[0], A.shape[0]))
+    for s in S:
+        best_action = 0
+        best_action_score = -9999999999999
+        for a in A:
+            tmp_sum = 0
+            for s_p in S:
+                tmp_sum += P[s, a, s_p, 0] * (
+                        P[s, a, s_p, 1] + gamma * V[s_p]
+                )
+            if tmp_sum > best_action_score:
+                best_action = a
+                best_action_score = tmp_sum
+        Pi[s] = 0.0
+        Pi[s, best_action] = 1.0
+    return V, Pi
