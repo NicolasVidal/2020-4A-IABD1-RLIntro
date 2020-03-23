a022626a4ca5b2eb06804a0516a4b1292062f17c
Nicolas Vidal <pro@nicolasvidal.fr>
2020-03-23 22:51:06 +0100

    Make line world and grid world generic

--- line_world/test_policy_evaluation.py
+++ line_world/test_policy_evaluation.py
@@ -5,16 +5,20 @@ from line_world import S, A, P, T
 from policies import tabular_uniform_random_policy
 
 if __name__ == "__main__":
+    import time
+
+    start_time = time.time()
     Pi = tabular_uniform_random_policy(S.shape[0], A.shape[0])
     V = iterative_policy_evaluation(S, A, P, T, Pi)
+    print("--- %s seconds ---" % (time.time() - start_time))
     print(V)
 
-    Pi = np.zeros((S.shape[0], A.shape[0]))
-    Pi[:, 1] = 1.0
-    V = iterative_policy_evaluation(S, A, P, T, Pi)
-    print(V)
-
-    Pi = np.zeros((S.shape[0], A.shape[0]))
-    Pi[:, 0] = 1.0
-    V = iterative_policy_evaluation(S, A, P, T, Pi)
-    print(V)
+    # Pi = np.zeros((S.shape[0], A.shape[0]))
+    # Pi[:, 1] = 1.0
+    # V = iterative_policy_evaluation(S, A, P, T, Pi)
+    # print(V)
+    #
+    # Pi = np.zeros((S.shape[0], A.shape[0]))
+    # Pi[:, 0] = 1.0
+    # V = iterative_policy_evaluation(S, A, P, T, Pi)
+    # print(V)
