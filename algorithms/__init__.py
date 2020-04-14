334f006fc24532e69694277d5ad861ad3e5bd4e2
Nicolas Vidal <pro@nicolasvidal.fr>
2020-04-14 15:06:59 +0200

    Add on_policy_monte_carlo_control (epsilon greedy policy) and off_policy_monte_carlo_control

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -208,3 +208,98 @@ def monte_carlo_with_exploring_starts_control(
             pi[st, :] = 0.0
             pi[st, np.argmax(q[st, :])] = 1.0
     return q, pi
+
+
+def on_policy_first_visit_monte_carlo_control(
+        states_count: int,
+        actions_count: int,
+        reset_func: Callable,
+        is_terminal_func: Callable,
+        step_func: Callable,
+        episodes_count: int = 10000,
+        max_steps_per_episode: int = 10,
+        epsilon: float = 0.2,
+        gamma: float = 0.99,
+) -> (np.ndarray, np.ndarray):
+    states = np.arange(states_count)
+    pi = tabular_uniform_random_policy(states_count, actions_count)
+    q = np.random.random((states_count, actions_count))
+    for s in states:
+        if is_terminal_func(s):
+            q[s, :] = 0.0
+            pi[s, :] = 0.0
+
+    returns = np.zeros((states_count, actions_count))
+    returns_count = np.zeros((states_count, actions_count))
+    for episode_id in range(episodes_count):
+        s0 = reset_func()
+
+        s_list, a_list, _, r_list = step_until_the_end_of_the_episode_and_return_history(s0, pi, is_terminal_func,
+                                                                                         step_func,
+                                                                                         max_steps_per_episode)
+
+        G = 0
+        for t in reversed(range(len(s_list))):
+            G = gamma * G + r_list[t]
+            st = s_list[t]
+            at = a_list[t]
+
+            if (st, at) in zip(s_list[0:t], a_list[0:t]):
+                continue
+            returns[st, at] += G
+            returns_count[st, at] += 1
+            q[st, at] = returns[st, at] / returns_count[st, at]
+            pi[st, :] = epsilon / actions_count
+            pi[st, np.argmax(q[st, :])] = 1.0 - epsilon + epsilon / actions_count
+    return q, pi
+
+
+def off_policy_monte_carlo_control(
+        states_count: int,
+        actions_count: int,
+        reset_func: Callable,
+        is_terminal_func: Callable,
+        step_func: Callable,
+        episodes_count: int = 10000,
+        max_steps_per_episode: int = 10,
+        epsilon: float = 0.2,
+        gamma: float = 0.99,
+) -> (np.ndarray, np.ndarray):
+    states = np.arange(states_count)
+    b = tabular_uniform_random_policy(states_count, actions_count)
+    pi = np.zeros((states_count, actions_count))
+    C = np.zeros((states_count, actions_count))
+    q = np.random.random((states_count, actions_count))
+    for s in states:
+        if is_terminal_func(s):
+            q[s, :] = 0.0
+            pi[s, :] = 0.0
+        pi[s, :] = 0.0
+        pi[s, np.argmax(q[s, :])] = 1.0
+
+    for episode_id in range(episodes_count):
+        s0 = reset_func()
+
+        s_list, a_list, _, r_list = step_until_the_end_of_the_episode_and_return_history(s0, b, is_terminal_func,
+                                                                                         step_func,
+                                                                                         max_steps_per_episode)
+
+        G = 0
+        W = 1
+        for t in reversed(range(len(s_list))):
+            G = gamma * G + r_list[t]
+            st = s_list[t]
+            at = a_list[t]
+
+            C[st, at] += W
+
+            q[st, at] += W / C[st, at] * (G - q[st, at])
+            pi[st, :] = 0.0
+            pi[st, np.argmax(q[st, :])] = 1.0
+
+            if at != np.argmax(q[st, :]):
+                break
+
+            W = W / b[st, at]
+
+    return q, pi
--- grid_world/__init__.py
+++ grid_world/__init__.py
@@ -31,3 +31,18 @@ P[num_states - 1, :, :, 0] = 0.0
 
 P[:, :, width - 1, 1] = -5.0
 P[:, :, num_states - 1, 1] = 1.0
+
+
+def reset() -> int:
+    return 0
+
+
+def is_terminal(state: int) -> bool:
+    return state in T
+
+
+def step(state: int, a: int) -> (int, float, bool):
+    assert (state not in T)
+    s_p = np.random.choice(S, p=P[state, a, :, 0])
+    r = P[state, a, s_p, 1]
+    return s_p, r, (s_p in T)
